c955002192fa97002fd93681ec106174bd4f3aa3
willz <williamjzh75@gmail.com>
2021-11-25 08:24:53 +1300

    third commit

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,12 @@
-import * as firebase from "firebase"
 
-const firebaseConfig = {
-    apiKey: "AIzaSyD-VwQmJesPwqL2b3A4CXYpHddA3SxJGvQ",
-    authDomain: "mission-5-f1624.firebaseapp.com",
-    databaseURL: "https://mission-5-f1624-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "mission-5-f1624",
-    storageBucket: "mission-5-f1624.appspot.com",
-    messagingSenderId: "986053436978",
-    appId: "1:986053436978:web:b41a36ce93f37df11fbe2b"
-  };
-  
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
+
+
+
+
+
+
+
+
+
+
+
